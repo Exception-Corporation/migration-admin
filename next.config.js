9ca0018d58952e269304b456ebd36d5398e302f8
Edgar Castillo Vega <vayne.edgar@gmail.com>
2023-06-09 21:16:18 -0600

    chore*: initial config to the migration-admin frontend app [v1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const webpack = require("webpack");
-const { parsed: myEnv } = require("dotenv").config({});
+const webpack = require('webpack');
+const { parsed: myEnv } = require('dotenv').config({});
 
 const nextConfig = {
   reactStrictMode: true,
@@ -8,7 +8,7 @@ const nextConfig = {
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
     return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
